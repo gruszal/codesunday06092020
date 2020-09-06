4c10114dd2fa5dd6303073d8785f8daf8fcbc5d2
Alek Gruszczyński(L450-win) <gruszal@gmail.com>
2020-09-06 14:02:55 +0200

    trzeci blok

---
+++ imie3.py
@@ -0,0 +1,9 @@
+# Napisz skrypt, który:
+# Wczyta z klawiatury imię i wypisze na ekran komunikat "Cześć, <imię>!"
+# Rozszerzenie: jeśli użytkownik wpisze imię z małej litery, zmień ją na wielką
+
+imie = input('Wpisz imię: ')
+
+wiadomosc = f'Cześć {imie}!'
+
+print(wiadomosc)
