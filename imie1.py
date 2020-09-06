a4e62d106270b91773c7f86ffefb7940b48964a0
Alek Gruszczyński(L450-win) <gruszal@gmail.com>
2020-09-06 13:02:32 +0200

    przed drugą przerwą

---
+++ imie1.py
@@ -0,0 +1,7 @@
+# Napisz skrypt, który:
+# Wczyta z klawiatury imię i wypisze na ekran komunikat "Cześć, <imię>!"
+# Rozszerzenie: jeśli użytkownik wpisze imię z małej litery, zmień ją na wielką
+
+imie = input('Wpisz imię: ')
+
+print('Cześć,', imie, '!')
--- skrypt5.py
+++
@@ -1,3 +0,0 @@
-# Napisz skrypt, który:
-# Wczyta z klawiatury imię i wypisze na ekran komunikat "Cześć, <imię>!"
-# Rozszerzenie: jeśli użytkownik wpisze imię z małej litery, zmień ją na wielką
